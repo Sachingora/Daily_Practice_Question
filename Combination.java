2db472e6483cc6cb07aa8668b193248db90a8743
Sachin Gora <sachingora1234@gmail.com>
2024-04-30 02:09:14 +0530

    Update Combination.java

--- Combination.java
+++ Combination.java
@@ -1,23 +1,59 @@
+// import java.util.*;
+
+// public class Combination {
+//          public static long fact(int num) {
+//                   if (num == 1 || num == 0) {
+//                            return 1;
+//                   } else {
+//                            long res = 1;
+//                            for (int i = 1; i <= num; i++) {
+//                                     res = res * i;
+//                            }
+//                            return res;
+//                   }
+//          }
+
+//          public static long calCombination(int n, int r) {
+//                   if (n == r || r == 0) {
+//                            return 1;
+//                   } else {
+//                            return (fact(n) / (fact(r) * fact(n - r)));
+//                   }
+//          }
+
+//          public static void main(String[] args) {
+//                   int n, r;
+//                   Scanner sc = new Scanner(System.in);
+//                   System.out.println("Enter the value of n: ");
+//                   n = sc.nextInt();
+//                   System.out.println("Enter the value of r: ");
+//                   r = sc.nextInt();
+
+//                   long result = calCombination(n, r);
+//                   System.out.println("Result: " + result);
+//          }
+// }
 import java.util.*;
+import java.math.BigInteger;
 
 public class Combination {
-         public static long fact(int num) {
+         public static BigInteger fact(int num) {
                   if (num == 1 || num == 0) {
-                           return 1;
+                           return BigInteger.ONE;
                   } else {
-                           long res = 1;
+                           BigInteger res = BigInteger.ONE;
                            for (int i = 1; i <= num; i++) {
-                                    res = res * i;
+                                    res = res.multiply(BigInteger.valueOf(i));
                            }
                            return res;
                   }
          }
 
-         public static long calCombination(int n, int r) {
-                  if (n == r || r == 0) {
-                           return 1;
+         public static BigInteger calCombination(int n, int r) {
+                  if (n < r) {
+                           return BigInteger.ZERO;
                   } else {
-                           return (fact(n) / (fact(r) * fact(n - r)));
+                           return fact(n).divide(fact(r).multiply(fact(n - r)));
                   }
          }
 
@@ -29,7 +65,7 @@ public class Combination {
                   System.out.println("Enter the value of r: ");
                   r = sc.nextInt();
 
-                  long result = calCombination(n, r);
+                  BigInteger result = calCombination(n, r);
                   System.out.println("Result: " + result);
          }
 }
