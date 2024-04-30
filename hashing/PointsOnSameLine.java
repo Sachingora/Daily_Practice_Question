fe13ad1ee4ec43fcb519bb0ef816d70f077a88c7
Devendra Tiwari <dtiwari18@gmail.com>
2024-04-30 22:37:08 +0530

    Added Problem #1

---
+++ hashing/PointsOnSameLine.java
@@ -0,0 +1,11 @@
+
+/**
+ * Given two arrays of integers A and B describing a pair of (A[i], B[i]) coordinates in a 2D plane. 
+ * A[i] describe x coordinates of the ith point in the 2D plane, whereas B[i] describes the y-coordinate of the ith point in the 2D plane.
+ * Find and return the maximum number of points that lie on the same line.
+ */
+
+public class Solution {
+    public int solve(int[] A, int[] B) {
+    }
+}
